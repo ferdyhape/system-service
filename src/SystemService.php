0c14797aaec805e63b8ea1c20092f992ab5361cf
Ferdy Hahan Pradana <ferdyhahan5@gmail.com>
2025-03-02 22:05:05 +0700

    Initial Commit

---
+++ src/SystemService.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Ferdyhape\SystemService;
+
+use Illuminate\Support\Facades\Storage;
+
+class ServiceName
+{
+    public static function handleUploadFile($file, $folderName, $oldFile = null)
+    {
+        if ($file) {
+            if ($oldFile) {
+                self::deleteFile($oldFile);
+            }
+            return $file->store($folderName, 'public');
+        }
+        return $oldFile;
+    }
+
+    public static function deleteFile($filePath)
+    {
+        if ($filePath) {
+            Storage::disk('public')->delete($filePath);
+        }
+    }
+}
